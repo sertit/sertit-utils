80b88d21c5ad962fee97ce2bb402bbd47e296261
BRAUN REMI <remi.braun@unistra.fr>
2022-08-30 17:27:32 +0200

    v1.16.1

--- sertit/__init__.py
+++ sertit/__init__.py
@@ -20,7 +20,7 @@
 .. include:: ../README.md
 """
 
-__version__ = "1.16.0"
+__version__ = "1.16.1"
 __title__ = "sertit"
 __description__ = ("SERTIT python library for generic tools",)
 __author__ = "ICube-SERTIT"
