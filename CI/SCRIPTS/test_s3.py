c8471f03583456aac7ac9fecb89716fc84009a57
CORIAT BASTIEN <b.coriat@unistra.fr>
2024-04-22 15:17:42 +0200

    Fix test since precedence over AWS credentials location has changed
    Here is the order of precedence from least to greatest in Sertit utils
    (the last listed configuration variables override all other variables):
     1. AWS profile
     2. Given endpoint_url as function argument
     3. AWS environment variable
    
    Thus, endpoint used in test functions are override by GitLab AWS variables.
    Test functions have to override AWS environment variables AWS_S3_ENDPOINT,
    AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID
    with for example tempenv.

--- CI/SCRIPTS/test_s3.py
+++ CI/SCRIPTS/test_s3.py
@@ -25,7 +25,6 @@ from tempenv import tempenv
 from CI.SCRIPTS.script_utils import CI_SERTIT_S3
 from sertit import rasters
 from sertit.s3 import USE_S3_STORAGE, s3_env, temp_s3
-from sertit.unistra import UNISTRA_S3_ENPOINT
 
 
 def base_fct(value):
@@ -38,7 +37,7 @@ def base_fct(value):
     return 1
 
 
-@s3_env(default_endpoint=UNISTRA_S3_ENPOINT, use_s3_env_var=CI_SERTIT_S3)
+@s3_env(use_s3_env_var=CI_SERTIT_S3)
 def with_s3(variable_1, variable_2):
     return base_fct(1)
 
@@ -52,7 +51,7 @@ def test_s3():
     with tempenv.TemporaryEnvironment({USE_S3_STORAGE: "1", CI_SERTIT_S3: "1"}):
         # Test s3_env and define_s3_client (called inside)
 
-        with temp_s3(default_endpoint=UNISTRA_S3_ENPOINT):
+        with temp_s3():
             raster_path = AnyPath("s3://sertit-sertit-utils-ci").joinpath(
                 "DATA", "rasters", "raster.tif"
             )
@@ -68,7 +67,11 @@ def test_s3():
 
 
 def test_no_sign_request():
-    with temp_s3(endpoint="s3.us-west-2.amazonaws.com", no_sign_request=True):
+    with tempenv.TemporaryEnvironment(
+        {
+            "AWS_S3_ENDPOINT": "s3.us-west-2.amazonaws.com",
+        }
+    ), temp_s3(no_sign_request=True):
         path = AnyPath(
             "s3://sentinel-cogs/sentinel-s2-l2a-cogs/40/V/DR/2023/11/S2A_40VDR_20231114_0_L2A"
         )
@@ -80,10 +83,11 @@ def test_no_sign_request():
 def test_requester_pays():
     with tempenv.TemporaryEnvironment(
         {
+            "AWS_S3_ENDPOINT": "s3.eu-central-1.amazonaws.com",
             "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_S3_AWS_SECRET_ACCESS_KEY"),
             "AWS_ACCESS_KEY_ID": os.getenv("AWS_S3_AWS_ACCESS_KEY_ID"),
         }
-    ), temp_s3(endpoint="s3.eu-central-1.amazonaws.com", requester_pays=True):
+    ), temp_s3(requester_pays=True):
         path = AnyPath("s3://sentinel-s2-l1c/tiles/29/H/NA/2023/11/14/0/")
         assert path.exists()
         with rasterio.open(str(path / "B12.jp2")) as ds:
