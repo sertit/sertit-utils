044eeaefbb8701f8a44e9da7f7f307088199f838
BRAUN REMI <remi.braun@unistra.fr>
2024-09-25 10:01:58 +0200

    v1.42.1.dev0

--- sertit/__meta__.py
+++ sertit/__meta__.py
@@ -17,7 +17,7 @@
 """
 Sertit Utils library
 """
-__version__ = "1.42.0"
+__version__ = "1.42.1.dev0"
 __title__ = "sertit"
 __description__ = ("SERTIT python library for generic tools",)
 __author__ = "ICube-SERTIT"
